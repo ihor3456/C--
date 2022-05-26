71c12cf3dd6d52b2457f985a231c70ac94c55b12
ihor3456 <72383679+ihor3456@users.noreply.github.com>
2022-05-26 15:48:06 +0300

    Create cpa_lab_5_1_9_(1)-A.cpp

---
+++ cpa_lab_5_1_9_(1)-A.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+public:
+	string name;
+	int age;
+	string lastName;
+};
+string FirstLastName(Person* person)
+{
+	return person->name + " " + person->lastName;
+}
+void print(Person* person)
+{
+	cout << FirstLastName(person) << " is " << person->age << " years old" << endl;
+}
+
+
+int main()
+{
+	Person person;
+	person.name = "Ihor";
+	person.age = 18;
+	person.lastName = "Toroshanko";
+	cout << "Meet " << person.name << "\n";
+	print(&person); printf("\n");
+
+	return 0;
+}
