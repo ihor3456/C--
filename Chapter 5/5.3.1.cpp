8d0f275601fdd93a02e0c9367f43a629ed996ab9
ihor3456 <72383679+ihor3456@users.noreply.github.com>
2022-05-26 23:38:28 +0300

    5.3.1

--- Chapter 5/5.3.1.cpp
+++ Chapter 5/5.3.1.cpp
@@ -1,79 +1,88 @@
 ﻿#include <iostream>
 using namespace std;
+
 class FlightBooking {
 public:
 	FlightBooking(int id, int capacity, int reserved);
 	void printStatus();
-	void add(int reserved);
-	void cancel(int reserved);
-	void exit();
+	void set_reserved(int reserved);
+	void add(int n);
+	void cancel(int n);
 private:
 	int id;
 	int capacity;
 	int reserved;
 };
+void FlightBooking::set_reserved(int reserved) {
+	if (reserved < 0)
+		this->reserved = 0;
+	else if (reserved > this->capacity)
+		cout << "Cannot perform this operation \n";
+	else
+		this->reserved = reserved;
+}
+void FlightBooking::add(int n)
+{
+	if (n > 0)
+	{
+		this->set_reserved(this->reserved + n);
+	}
+}
+void FlightBooking::cancel(int n)
+{
+	if (this->reserved >= n)
+	{
+		this->set_reserved(this->reserved - n);
+	}
+}
 void FlightBooking::printStatus()
 {
 	double percent = 0;
-	if (reserved >= capacity) {
-		percent = 100;
-		cout << percent<<"% seats are ocupied\n";
-	}
-	else {
-		percent = 100.0 * ((double)this->reserved / this->capacity);
-		cout<< "Flight seats taken "<< " id " << this->id << " reserved " << this->reserved << " capacity " << this->capacity << " percentage " << percent << endl;
-	}
+	percent = 100 * ((double)reserved / capacity);
+	if (percent <= 105)
+		cout << "Flight " << id << " : " << reserved << "/" << capacity << " (" << percent << "%) " << " seats reserved \n";
+	else 
+		cout << "to much reservations";
 	// print report here
 }
-void FlightBooking::add(int reserved) {
-	if (this->reserved + reserved > capacity) {
-		reserved = 0;
-		cout << "this operation is impossible\n";
-	}
-	else {
-		this->reserved += reserved;
-		cout << "successfully added!\n";
-	}
-}
-void FlightBooking::cancel(int reserved) {
-	if (this->reserved - reserved > 0) {
-		this->reserved = this->reserved - reserved;
-		cout << "successfully canceled reservation\n";
-	}
-	else {
-		reserved = 0;
-		cout << "this operation is impossible\n";
-	}
-}
-void FlightBooking::exit() {
-	cout << "thank for choosing out booking company\n";
-	cout << "booking has finished\n";
-}
-
 FlightBooking::FlightBooking(int id, int capacity, int reserved)
-{	
+{
 	this->id = id;
 	this->capacity = capacity;
 	this->reserved = reserved;
-	if (this->reserved >= this->capacity) 
-	{
-		cout << "all seats are ocupied\n";
-	}
 	// Save data to members
 }
 int main() {
-	int reserved = 0,
-		capacity = 0;
+	int capacity = 0, reserved = 0;
 	cout << "Provide flight capacity: ";
 	cin >> capacity;
 	cout << "Provide number of reserved seats: ";
 	cin >> reserved;
 	FlightBooking booking(1, capacity, reserved);
+	string command;
+	while (true)
+	{
+		cout << "Enter a command: ";
+		cin >> command;
+		if (command == "quit")
+			break;
+		cout << "Enter the number: ";
+		int numb;
+		if (command == "add")
+		{
+			cin >> numb;
+			booking.add(numb);
+		}
+		else if (command == "cancel")
+		{
+			cin >> numb;
+			booking.cancel(numb);
+		}
+		else
+		{
+			cout<< "unknown command\n";
+		}
+	}
 	booking.printStatus();
-	booking.add(20);
-	booking.printStatus();
-	booking.cancel(60);
-	booking.printStatus();
-	booking.exit();
 	return 0;
 }
